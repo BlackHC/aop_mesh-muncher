5a86b68285172d6caeef0c1b7a23936b77808e16
Andreas Kirsch <blackhc@gmail.com>
2012-07-31 16:09:06 +0200

    extract the mouse capturing code into its own class MouseCapture

--- positionSolver/positionSolver.cpp
+++ positionSolver/positionSolver.cpp
@@ -271,57 +271,99 @@ std::shared_ptr<T> shared_from_stack(T &object) {
 	return std::shared_ptr<T>( &object, null_deleter() );
 }
 
-struct CameraInputControl : public EventHandler {
-	std::shared_ptr<Camera> camera;
-	std::shared_ptr<sf::Window> window;
-	sf::Vector2i lastMousePosition;
-	sf::Vector2i mouseDelta;
+struct MouseCapture : public EventHandler  {
+	typedef MouseCapture super;
 
-	bool active;
-	
-	void init( const std::shared_ptr<Camera> &camera, const std::shared_ptr<sf::Window> &window ) {
-		this->camera = camera;
+	std::shared_ptr<sf::Window> window;
+
+	MouseCapture() : captureMouse( false ) {}
+
+	void init( const std::shared_ptr<sf::Window> &window ) {
 		this->window = window;
-		lastMousePosition = sf::Mouse::getPosition();
-		active = false;
+	}
+	
+	bool getCapture() const {
+		return captureMouse;
+	}
+
+	sf::Vector2i getMouseDelta() {
+		sf::Vector2i temp = mouseDelta;
+		mouseDelta = sf::Vector2i();
+		return temp;
+	}
+
+	void setCapture( bool active ) {
+		if( captureMouse == active ) {
+			return;
+		}
+
+		mouseDelta = sf::Vector2i();
+
+		if( active ) {
+			oldMousePosition = sf::Mouse::getPosition();
+		}
+		// TODO: add support for ClipCursor?
+		window->setMouseCursorVisible( !active );
+		
+		captureMouse = active;
 	}
 
 	bool handleEvent( const sf::Event &event ) {
 		switch( event.type ) {
+		case sf::Event::MouseLeft:
+			if( captureMouse ) {
+				sf::Mouse::setPosition( sf::Vector2i( window->getSize() / 2u ), *window );	
+				oldMousePosition = sf::Mouse::getPosition();
+			}
+			return true;
+		case sf::Event::MouseMoved:
+			if( captureMouse ) {
+				mouseDelta += sf::Mouse::getPosition() - oldMousePosition;
+				oldMousePosition = sf::Mouse::getPosition();
+			}			
+			return true;
+		}
+		return false;
+	}
+
+private:
+	sf::Vector2i mouseDelta, oldMousePosition;
+	bool captureMouse;
+};
+
+struct CameraInputControl : public MouseCapture {
+	std::shared_ptr<Camera> camera;
+
+	void init( const std::shared_ptr<Camera> &camera, const std::shared_ptr<sf::Window> &window ) {
+		super::init( window );
+		this->camera = camera;
+	}
+
+	bool handleEvent( const sf::Event &event ) {
+		if( super::handleEvent( event ) ) {
+			return true;
+		}
+
+		switch( event.type ) {
 		case sf::Event::LostFocus:
-			active = false;
-			window->setMouseCursorVisible( true );
+			setCapture( true );
 			break;
 		case sf::Event::KeyPressed:
 			if( event.key.code == sf::Keyboard::Escape ) {
-				active = false;
-				window->setMouseCursorVisible( true );
+				setCapture( false );
 			}
 			return true;
 		case sf::Event::MouseButtonReleased:
 			if( event.mouseButton.button == sf::Mouse::Left ) {
-				active = true;
-				window->setMouseCursorVisible( false );
+				setCapture( true );
 			}
-			return true;
-		case sf::Event::MouseLeft:
-			if( active ) {
-				sf::Mouse::setPosition( sf::Vector2i( window->getSize() / 2u ), *window );	
-				lastMousePosition = sf::Mouse::getPosition();
-			}
-			return true;
-		case sf::Event::MouseMoved:
-			if( active ) {
-				mouseDelta += sf::Mouse::getPosition() - lastMousePosition;
-			}
-			lastMousePosition = sf::Mouse::getPosition();
 			return true;
 		}
 		return false;
 	}
 	
 	bool update( const float elapsedTime, bool inputProcessed ) {
-		if( !inputProcessed && active ) {
+		if( !inputProcessed && getCapture() ) {
 			Eigen::Vector3f relativeMovement = Vector3f::Zero();
 			if( sf::Keyboard::isKeyPressed( sf::Keyboard::W ) ) {
 				relativeMovement.z() -= 1;
@@ -347,8 +389,7 @@ struct CameraInputControl : public EventHandler {
 			Eigen::Vector3f newPosition = camera->getPosition() + camera->getViewTransformation().linear().transpose() * relativeMovement;
 			camera->setPosition( newPosition );
 
-			sf::Vector2f angleDelta = sf::Vector2f( mouseDelta ) * float(Math::PI / 180.0) * 0.5f;
-			mouseDelta = sf::Vector2i();
+			sf::Vector2f angleDelta = sf::Vector2f( getMouseDelta() ) * float(Math::PI / 180.0) * 0.5f;
 
 			camera->setOrientation( 
 				Eigen::AngleAxisf( angleDelta.x, Eigen::Vector3f::Unit(1) ) *
