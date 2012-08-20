7486c52931761900327adbc8662195fb83d66260
Andreas Kirsch <blackhc@gmail.com>
2012-08-20 11:13:25 +0200

    positionSolver: rewrite solveIntersections into solveIntersectionsWithPriority

--- positionSolver/positionSolver.cpp
+++ positionSolver/positionSolver.cpp
@@ -1,409 +1,24 @@
 #include <iostream>
-#include <numeric>
-#include <algorithm>
+
 #include <functional>
 #include <iterator>
 
-#include <gtest.h>
+#include <algorithm>
 #include <Eigen/Eigen>
+#include <memory>
+
+#include <gtest.h>
 
 #include "debugRender.h"
 
 #include <SFML/Window.hpp>
 
-#include <memory>
-
-using namespace Eigen;
-
-struct Point {
-	Vector3f center;
-	float distance;
-
-	Point( const Vector3f &center, float distance ) : center( center ), distance( distance ) {}
-};
-
-struct Grid {
-	Vector3i size;
-	int count;
-
-	Vector3f offset;
-	float resolution;
-
-	Grid( const Vector3i &size, const Vector3f &offset, float resolution ) : size( size ), offset( offset ), resolution( resolution ) {
-		count = size[0] * size[1] * size[2];
-	}
-
-	int getIndex( const Vector3i &index3 ) {
-		return index3[0] + size[0] * index3[1] + (size[0] * size[1]) * index3[2];
-	}
-
-	Vector3i getIndex3( int index ) {
-		int x = index % size[0];
-		index /= size[0];
-		int y = index % size[1];
-		index /= size[1];
-		int z = index;
-		return Vector3i( x,y,z );
-	}
-
-	Vector3f getPosition( const Vector3i &index3 ) {
-		return offset + index3.cast<float>() * resolution;
-	}
-};
-
-const Vector3i indexToCubeCorner[] = {
-	Vector3i( 0,0,0 ), Vector3i( 1,0,0 ), Vector3i( 0,1,0 ), Vector3i( 0,0,1 ),
-	Vector3i( 1,1,0 ), Vector3i( 0,1,1 ), Vector3i( 1,0,1 ), Vector3i( 1,1,1 )
-};
-
-inline float squaredMinDistanceAABoxPoint( const Vector3f &min, const Vector3f &max, const Vector3f &point ) {
-	Vector3f distance;
-	for( int i = 0 ; i < 3 ; i++ ) {
-		if( point[i] > max[i] ) {
-			distance[i] = point[i] - max[i];
-		}
-		else if( point[i] > min[i] ) {
-			distance[i] = 0.f;
-		}
-		else {
-			distance[i] = min[i] - point[i];
-		}
-	}
-	return distance.squaredNorm();
-}
-
-inline float squaredMaxDistanceAABoxPoint( const Vector3f &min, const Vector3f &max, const Vector3f &point ) {
-	Vector3f distanceA = (point - min).cwiseAbs();
-	Vector3f distanceB = (point - max).cwiseAbs();
-	Vector3f distance = distanceA.cwiseMax( distanceB );
-
-	return distance.squaredNorm();
-}
-
-struct SparseCellInfo {
-	Vector3f minCorner;
-	Vector3f maxCorner;
-
-	// upper bound
-	int totalCount;
-	// lower bound
-	int fullCount;
-
-	std::vector<int> fullPointIndices;
-	std::vector<int> partialPointIndices;
-
-	SparseCellInfo() : totalCount( 0 ), fullCount( 0 ) {}
-	SparseCellInfo( SparseCellInfo &&c ) : minCorner( c.minCorner), maxCorner( c.maxCorner ), totalCount( c.totalCount ),
-		fullCount( c.fullCount ), fullPointIndices( std::move( c.fullPointIndices ) ), partialPointIndices( std::move( c.partialPointIndices ) ) {}
-};
-
-std::vector<std::vector<SparseCellInfo>> solveIntersectionsFromPigeonLevel( const std::vector<Point> &points, float halfThickness ) {
-	// advantage: starts at intermediate level
-	// disadvantage: if there are many points, an additional acceleration structure is required for the first step
-	std::vector<std::vector<SparseCellInfo>> results;
-
-	Vector3f minGridCorner = std::accumulate( points.cbegin(), points.cend(), points[0].center, [] (const Vector3f &x, const Point &p) { return x.cwiseMin( p.center - Vector3f::Constant( p.distance ) ); } );
-	Vector3f maxGridCorner = std::accumulate( points.cbegin(), points.cend(), points[0].center, [] (const Vector3f &x, const Point &p) { return x.cwiseMax( p.center + Vector3f::Constant( p.distance ) ); } );
-
-#if 0
-	float resolution = (maxGridCorner - minGridCorner).maxCoeff();
-	Grid grid( Vector3i::Constant( 1 ), minGridCorner, resolution );
-#else
-	// start with a resolution that guarantees that there will be full matches
-	float resolution = halfThickness * 0.707106; // rounded down
-	Vector3i size = ((maxGridCorner - minGridCorner) / resolution + Vector3f::Constant(1)).cast<int>();
-	Grid grid( size, minGridCorner, resolution );
-#endif
-
-	int bestFullCount = 0;
-	int bestTotalCount = 0;
-
-	std::vector<SparseCellInfo> sparseCellInfos;
-
-	for( int i = 0 ; i < grid.count ; i++ ) {
-		Vector3f minCorner = grid.getPosition( grid.getIndex3(i) );
-		Vector3f maxCorner = minCorner + Vector3f::Constant( resolution );
-
-		SparseCellInfo cellInfo;
-
-		for( int pointIndex = 0 ; pointIndex < points.size() ; pointIndex++ ) {
-			const Point &point = points[pointIndex];
-			float minSquaredDistance = squaredMinDistanceAABoxPoint( minCorner, maxCorner, point.center );
-			float maxSquaredDistance = squaredMaxDistanceAABoxPoint( minCorner, maxCorner, point.center );
-
-			float minSphereSquaredDistance = (point.distance - halfThickness) * (point.distance - halfThickness);
-			float maxSphereSquaredDistance = (point.distance + halfThickness) * (point.distance + halfThickness);
-			if( maxSquaredDistance < minSphereSquaredDistance || maxSphereSquaredDistance < minSquaredDistance ) {
-				// no intersection
-				continue;
-			}
-			// at least partial
-			cellInfo.totalCount++;
-
-			// full?
-			if( minSphereSquaredDistance <= minSquaredDistance && maxSquaredDistance <= maxSphereSquaredDistance ) {
-				cellInfo.fullCount++;
-				cellInfo.fullPointIndices.push_back(pointIndex);
-			}
-			else {
-				// only partial
-				cellInfo.partialPointIndices.push_back(pointIndex);
-			}
-		}
-
-		// only keep potential solutions
-		if( cellInfo.totalCount >= bestFullCount && cellInfo.totalCount > 0 ) {
-			cellInfo.minCorner = minCorner;
-			cellInfo.maxCorner = maxCorner;
-
-			bestTotalCount = std::max( bestTotalCount, cellInfo.totalCount );
-			bestFullCount = std::max( bestFullCount, cellInfo.fullCount );
-
-			sparseCellInfos.push_back( std::move( cellInfo ) );
-		}
-	}
-
-	// filter
-	if( bestFullCount > 0 ) {
-		std::vector<SparseCellInfo> filteredSparseCellInfos;
-		filteredSparseCellInfos.reserve( sparseCellInfos.size() );
-
-		std::remove_copy_if( sparseCellInfos.begin(), sparseCellInfos.end(), std::back_inserter( filteredSparseCellInfos ), [bestFullCount](SparseCellInfo &info) { return info.totalCount < bestFullCount; } );
-		std::swap( filteredSparseCellInfos, sparseCellInfos );
-	}
-
-	// done?
-	if( bestFullCount == bestTotalCount ) {
-		// solution found
-		results.push_back( std::move( sparseCellInfos ) );
-		return results;
-	}
-
-	// refine
-	for( int refineStep = 0 ; refineStep < 8 ; ++refineStep ) {
-		resolution /= 2;
-
-		std::vector<SparseCellInfo> refinedSparseCellInfos;
-		refinedSparseCellInfos.reserve( sparseCellInfos.size() * 8 );
-
-		// reset total count to lowest possible value (ie bestFullCount)
-		bestTotalCount = bestFullCount;
-
-		for( int i = 0 ; i < sparseCellInfos.size() ; ++i ) {
-			const SparseCellInfo &parentCellInfo = sparseCellInfos[i];
-
-			// early out if there is nothing to refine
-			if( parentCellInfo.partialPointIndices.empty() ) {
-				refinedSparseCellInfos.push_back( parentCellInfo );
-				continue;
-			}
-
-			for( int k = 0 ; k < 8 ; k++ ) {
-				Vector3f offset = indexToCubeCorner[k].cast<float>() * resolution;
-
-				Vector3f minCorner = parentCellInfo.minCorner + offset;
-				Vector3f maxCorner = minCorner + Vector3f::Constant( resolution );
-
-				SparseCellInfo cellInfo;
-				// full overlaps in the parent are also full overlaps in the refined child
-				cellInfo.fullPointIndices = parentCellInfo.fullPointIndices;
-				cellInfo.fullCount = parentCellInfo.fullCount;
-
-				// reset total count to lowest possible value (ie bestFullCount)
-				cellInfo.totalCount = cellInfo.fullCount;
-
-				// check which partial overlaps become full overlaps in the refined child
-				for( int j = 0 ; j < parentCellInfo.partialPointIndices.size() ; j++ ) {
-					const int pointIndex = parentCellInfo.partialPointIndices[j];
-
-					const Point &point = points[pointIndex];
-					float minSquaredDistance = squaredMinDistanceAABoxPoint( minCorner, maxCorner, point.center );
-					float maxSquaredDistance = squaredMaxDistanceAABoxPoint( minCorner, maxCorner, point.center );
-
-					float minSphereSquaredDistance = (point.distance - halfThickness) * (point.distance - halfThickness);
-					float maxSphereSquaredDistance = (point.distance + halfThickness) * (point.distance + halfThickness);
-					if( maxSquaredDistance < minSphereSquaredDistance || maxSphereSquaredDistance < minSquaredDistance ) {
-						continue;
-					}
-					// at least partial
-					cellInfo.totalCount++;
-
-					// full?
-					if( minSphereSquaredDistance <= minSquaredDistance && maxSquaredDistance <= maxSphereSquaredDistance ) {
-						cellInfo.fullCount++;
-						cellInfo.fullPointIndices.push_back( pointIndex );
-					}
-					else {
-						// only partial
-						cellInfo.partialPointIndices.push_back( pointIndex );
-					}
-				}
-
-				// only keep potential solutions
-				if( cellInfo.totalCount >= bestFullCount && cellInfo.totalCount > 0 ) {
-					cellInfo.minCorner = minCorner;
-					cellInfo.maxCorner = maxCorner;
-
-					bestTotalCount = std::max( bestTotalCount, cellInfo.totalCount );
-					bestFullCount = std::max( bestFullCount, cellInfo.fullCount );
-
-					refinedSparseCellInfos.push_back( std::move( cellInfo ) );
-				}
-			}
-		}
-
-		std::swap( sparseCellInfos, refinedSparseCellInfos );
-
-		results.push_back( std::move( refinedSparseCellInfos ) );
-
-		// filter
-		if( bestFullCount > 0 ) {
-			std::vector<SparseCellInfo> filteredSparseCellInfos;
-			filteredSparseCellInfos.reserve( sparseCellInfos.size() );
-
-			std::remove_copy_if( sparseCellInfos.begin(), sparseCellInfos.end(), std::back_inserter( filteredSparseCellInfos ), [bestFullCount](SparseCellInfo &info) { return info.totalCount < bestFullCount; } );
-			std::swap( filteredSparseCellInfos, sparseCellInfos );
-		}
-
-		// done?
-		if( bestFullCount == bestTotalCount ) {
-			// solution found
-			results.push_back( std::move( sparseCellInfos ) );
-			return results;
-		}
-	}
-
-	results.push_back( std::move( sparseCellInfos ) );
-	return results;
-}
-
-std::vector<std::vector<SparseCellInfo>> solveIntersections( const std::vector<Point> &points, float halfThickness ) {
-	// advantage: if there are many points, an additional acceleration structure is required for the first step
-	// disadvantage: no meaning full results during the first (possibly many steps until resolution <= halfThickness * 0.707106)
-	std::vector<std::vector<SparseCellInfo>> results;
-
-	Vector3f minGridCorner = std::accumulate( points.cbegin(), points.cend(), points[0].center, [] (const Vector3f &x, const Point &p) { return x.cwiseMin( p.center - Vector3f::Constant( p.distance ) ); } );
-	Vector3f maxGridCorner = std::accumulate( points.cbegin(), points.cend(), points[0].center, [] (const Vector3f &x, const Point &p) { return x.cwiseMax( p.center + Vector3f::Constant( p.distance ) ); } );
-
-	float resolution = (maxGridCorner - minGridCorner).maxCoeff();
-	//Grid grid( Vector3i::Constant( 1 ), minGridCorner, resolution );
-
-	int bestFullCount = 0;
-	int bestTotalCount = points.size();
-
-	std::vector<SparseCellInfo> sparseCellInfos;
-
-	SparseCellInfo gridCell;
-	gridCell.minCorner = minGridCorner;
-	gridCell.maxCorner = minGridCorner + Vector3f::Constant( resolution );
-	gridCell.fullCount = 0;
-	gridCell.totalCount = points.size();
-	gridCell.partialPointIndices.resize( points.size() );
-	for( int i = 0 ; i < points.size() ; ++i ) {
-		gridCell.partialPointIndices[i] = i;
-	}
-	sparseCellInfos.push_back( gridCell );
-
-	// refine
-	for( int refineStep = 0 ; refineStep < 16 ; ++refineStep ) {
-		resolution /= 2;
-
-		std::vector<SparseCellInfo> refinedSparseCellInfos;
-		refinedSparseCellInfos.reserve( sparseCellInfos.size() * 8 );
-
-		// reset total count to lowest possible value (ie bestFullCount)
-		bestTotalCount = bestFullCount;
-
-		for( int i = 0 ; i < sparseCellInfos.size() ; ++i ) {
-			const SparseCellInfo &parentCellInfo = sparseCellInfos[i];
-
-			// early out if there is nothing to refine
-			if( parentCellInfo.partialPointIndices.empty() ) {
-				refinedSparseCellInfos.push_back( parentCellInfo );
-				continue;
-			}
-
-			for( int k = 0 ; k < 8 ; k++ ) {
-				Vector3f offset = indexToCubeCorner[k].cast<float>() * resolution;
-
-				Vector3f minCorner = parentCellInfo.minCorner + offset;
-				Vector3f maxCorner = minCorner + Vector3f::Constant( resolution );
-
-				SparseCellInfo cellInfo;
-				// full overlaps in the parent are also full overlaps in the refined child
-				cellInfo.fullPointIndices = parentCellInfo.fullPointIndices;
-				cellInfo.fullCount = parentCellInfo.fullCount;
-
-				// reset total count to lowest possible value (ie bestFullCount)
-				cellInfo.totalCount = cellInfo.fullCount;
-
-				// check which partial overlaps become full overlaps in the refined child
-				for( int j = 0 ; j < parentCellInfo.partialPointIndices.size() ; j++ ) {
-					const int pointIndex = parentCellInfo.partialPointIndices[j];
-
-					const Point &point = points[pointIndex];
-					float minSquaredDistance = squaredMinDistanceAABoxPoint( minCorner, maxCorner, point.center );
-					float maxSquaredDistance = squaredMaxDistanceAABoxPoint( minCorner, maxCorner, point.center );
-
-					float minSphereSquaredDistance = (point.distance - halfThickness) * (point.distance - halfThickness);
-					float maxSphereSquaredDistance = (point.distance + halfThickness) * (point.distance + halfThickness);
-					if( maxSquaredDistance < minSphereSquaredDistance || maxSphereSquaredDistance < minSquaredDistance ) {
-						continue;
-					}
-					// at least partial
-					cellInfo.totalCount++;
-
-					// full?
-					if( minSphereSquaredDistance <= minSquaredDistance && maxSquaredDistance <= maxSphereSquaredDistance ) {
-						cellInfo.fullCount++;
-						cellInfo.fullPointIndices.push_back( pointIndex );
-					}
-					else {
-						// only partial
-						cellInfo.partialPointIndices.push_back( pointIndex );
-					}
-				}
-
-				// only keep potential solutions
-				if( cellInfo.totalCount >= bestFullCount && cellInfo.totalCount > 0 ) {
-					cellInfo.minCorner = minCorner;
-					cellInfo.maxCorner = maxCorner;
-
-					bestTotalCount = std::max( bestTotalCount, cellInfo.totalCount );
-					bestFullCount = std::max( bestFullCount, cellInfo.fullCount );
-
-					refinedSparseCellInfos.push_back( std::move( cellInfo ) );
-				}
-			}
-		}
-
-		std::swap( sparseCellInfos, refinedSparseCellInfos );
-
-		results.push_back( std::move( refinedSparseCellInfos ) );
-
-		// filter
-		if( bestFullCount > 0 ) {
-			std::vector<SparseCellInfo> filteredSparseCellInfos;
-			filteredSparseCellInfos.reserve( sparseCellInfos.size() );
-
-			std::remove_copy_if( sparseCellInfos.begin(), sparseCellInfos.end(), std::back_inserter( filteredSparseCellInfos ), [bestFullCount](SparseCellInfo &info) { return info.totalCount < bestFullCount; } );
-			std::swap( filteredSparseCellInfos, sparseCellInfos );
-		}
-
-		// done?
-		if( bestFullCount == bestTotalCount ) {
-			// solution found
-			results.push_back( std::move( sparseCellInfos ) );
-			return results;
-		}
-	}
-
-	results.push_back( std::move( sparseCellInfos ) );
-	return results;
-}
+#include "positionSolver.h"
 
 #include "camera.h"
 #include "eventHandling.h"
+
+using namespace Eigen;
 
 struct null_deleter {
 	template<typename T>
@@ -547,6 +162,10 @@ void main() {
 	std::vector<Point> points;
 
 	points.push_back( Point( Vector3f(0,0,0), 5 ) );
+	points.push_back( Point( Vector3f(0,0,0), 5 ) );
+	points.push_back( Point( Vector3f(0,0,0), 5 ) );
+	points.push_back( Point( Vector3f(0,0,0), 5 ) );
+
 	points.push_back( Point( Vector3f(10,0,0), 5 ) );
 	points.push_back( Point( Vector3f(5,0,5), 5 ) );
 
@@ -558,18 +177,18 @@ void main() {
 	points.push_back( Point( Vector3f(10,6.5,0), 5 ) );
 	points.push_back( Point( Vector3f(5,6.5,5), 5 ) );
 
-	/*points.push_back( Point( Vector3f(0,0,0), 5 ) );
-	points.push_back( Point( Vector3f(11.8,0,0), 5 ) );*/
+	points.push_back( Point( Vector3f(0,0,0), 5 ) );
+	points.push_back( Point( Vector3f(11.8,0,0), 5 ) );
 
 	const float halfThickness = 1;
-	auto results = solveIntersections( points, halfThickness );
+	auto results = solveIntersectionsWithPriority( points, halfThickness, 0.1 );
 
 	sf::Window window( sf::VideoMode( 640, 480 ), "Position Solver", sf::Style::Default, sf::ContextSettings(32) );
 
 	Camera camera;
 	camera.perspectiveProjectionParameters.aspect = 640.0 / 480.0;
 	camera.perspectiveProjectionParameters.FoV_y = 75.0;
-	camera.perspectiveProjectionParameters.zNear = 1.0;
+	camera.perspectiveProjectionParameters.zNear = 0.0001f;
 	camera.perspectiveProjectionParameters.zFar = 500.0;
 	//camera.position.z() = 5;
 
@@ -594,19 +213,13 @@ void main() {
 	}
 	debugSourcePoints.end();
 
-	std::vector<DebugRender::CombinedCalls> debugResults;
-	debugResults.resize( results.size() );
-	int i = 0;
-	for( auto sparseCellsLevel = results.begin() ; sparseCellsLevel != results.end() ; ++sparseCellsLevel, ++i ) {
-		debugResults[i].begin();
-		for( auto sparseCellInfo = sparseCellsLevel->begin() ; sparseCellInfo != sparseCellsLevel->end() ; ++sparseCellInfo ) {
-			debugResults[i].setColor( Eigen::Vector3f( 1.0, 0.0, 0.0 ) * ( float(sparseCellInfo->fullCount) / sparseCellInfo->totalCount * 0.75 + 0.25 ) );
-			debugResults[i].drawAABB( sparseCellInfo->minCorner, sparseCellInfo->maxCorner );
-		}
-		debugResults[i].end();
+	DebugRender::CombinedCalls debugResults;
+	debugResults.begin();
+	for( auto sparseCellInfo = results.begin() ; sparseCellInfo != results.end() ; ++sparseCellInfo ) {
+		debugResults.setColor( Eigen::Vector3f( 1.0, 0.0, 0.0 ) * ( float(sparseCellInfo->upperBound + sparseCellInfo->lowerBound) / 2 / points.size() * 0.75 + 0.25 ) );
+		debugResults.drawAABB( sparseCellInfo->minCorner, sparseCellInfo->minCorner + Vector3f::Constant( sparseCellInfo->resolution ) );
 	}
-
-	int level = results.size() - 1;
+	debugResults.end();
 
 	// The main loop - ends as soon as the window is closed
 	sf::Clock frameClock, clock;
@@ -626,15 +239,6 @@ void main() {
 			}
 
 			cameraInputControl.handleEvent( event );
-
-			if( event.type == sf::Event::KeyPressed ) {
-				if( event.key.code == sf::Keyboard::Up ) {
-					level = (level + 1) % results.size();
-				}
-				if( event.key.code == sf::Keyboard::Down ) {
-					level = (level - 1 + results.size()) % results.size();
-				}
-			}
 		}
 
 		cameraInputControl.update( frameClock.restart().asSeconds(), false );
@@ -652,7 +256,7 @@ void main() {
 		glLoadMatrix( camera.getViewTransformation().matrix() );
 
 		debugSourcePoints.render();
-		debugResults[level].render();
+		debugResults.render();
 
 		// End the current frame and display its contents on screen
 		window.display();
---
+++ positionSolver/positionSolver.h
@@ -0,0 +1,220 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include <Eigen/Eigen>
+#include <numeric>
+
+struct Point {
+	Eigen::Vector3f center;
+	float distance;
+
+	Point( const Eigen::Vector3f &center, float distance ) : center( center ), distance( distance ) {}
+};
+
+const Eigen::Vector3i indexToCubeCorner[] = {
+	Eigen::Vector3i( 0,0,0 ), Eigen::Vector3i( 1,0,0 ), Eigen::Vector3i( 0,1,0 ), Eigen::Vector3i( 0,0,1 ),
+	Eigen::Vector3i( 1,1,0 ), Eigen::Vector3i( 0,1,1 ), Eigen::Vector3i( 1,0,1 ), Eigen::Vector3i( 1,1,1 )
+};
+
+inline float squaredMinDistanceAABoxPoint( const Eigen::Vector3f &min, const Eigen::Vector3f &max, const Eigen::Vector3f &point ) {
+	Eigen::Vector3f distance;
+	for( int i = 0 ; i < 3 ; i++ ) {
+		if( point[i] > max[i] ) {
+			distance[i] = point[i] - max[i];
+		}
+		else if( point[i] > min[i] ) {
+			distance[i] = 0.f;
+		}
+		else {
+			distance[i] = min[i] - point[i];
+		}
+	}
+	return distance.squaredNorm();
+}
+
+inline float squaredMaxDistanceAABoxPoint( const Eigen::Vector3f &min, const Eigen::Vector3f &max, const Eigen::Vector3f &point ) {
+	Eigen::Vector3f distanceA = (point - min).cwiseAbs();
+	Eigen::Vector3f distanceB = (point - max).cwiseAbs();
+	Eigen::Vector3f distance = distanceA.cwiseMax( distanceB );
+
+	return distance.squaredNorm();
+}
+
+struct SparseCellInfo {
+	Eigen::Vector3f minCorner;
+	float resolution;
+
+	// # potential/partial overlaps
+	int upperBound;
+	// # full overlaps
+	int lowerBound;
+
+	// debug-only
+	//std::vector<int> fullPointIndices;
+	std::vector<int> partialPointIndices;
+
+	SparseCellInfo() : upperBound( 0 ), lowerBound( 0 ) {}
+	SparseCellInfo( SparseCellInfo &&c ) : minCorner( c.minCorner), resolution( c.resolution ), upperBound( c.upperBound ),
+		lowerBound( c.lowerBound ), /*fullPointIndices( std::move( c.fullPointIndices ) ),*/ partialPointIndices( std::move( c.partialPointIndices ) ) {}
+};
+
+SparseCellInfo buildRootCell( const std::vector<Point> &points, const float halfThickness ) {
+	const Eigen::Vector3f minGridCorner = std::accumulate( points.cbegin(), points.cend(), points[0].center, [] (const Eigen::Vector3f &x, const Point &p) { return x.cwiseMin( p.center - Eigen::Vector3f::Constant( p.distance ) ); } ) - Eigen::Vector3f::Constant( halfThickness );
+	const Eigen::Vector3f maxGridCorner = std::accumulate( points.cbegin(), points.cend(), points[0].center, [] (const Eigen::Vector3f &x, const Point &p) { return x.cwiseMax( p.center + Eigen::Vector3f::Constant( p.distance ) ); } ) + Eigen::Vector3f::Constant( halfThickness );
+
+	const float gridResolution = (maxGridCorner - minGridCorner).maxCoeff();
+
+	SparseCellInfo rootCell;
+	rootCell.minCorner = minGridCorner;
+	rootCell.resolution = gridResolution;
+
+	rootCell.lowerBound = 0;
+	rootCell.upperBound = points.size();
+
+	rootCell.partialPointIndices.resize( points.size() );
+	for( int i = 0 ; i < points.size() ; ++i ) {
+		rootCell.partialPointIndices[i] = i;
+	}
+
+	return rootCell;
+}
+
+void filterCells( std::vector<SparseCellInfo> &cells, const std::function<bool(const SparseCellInfo &)> &remove_if ) {
+	std::vector<SparseCellInfo> filteredCells;
+
+	filteredCells.reserve( cells.size() );
+
+	std::remove_copy_if( cells.begin(), cells.end(), std::back_inserter( filteredCells ), remove_if );
+	std::swap( filteredCells, cells );
+}
+
+std::vector<SparseCellInfo> solveIntersectionsWithPriority( const std::vector<Point> &points, const float halfThickness, const float minResolution ) {
+	SparseCellInfo rootCell = buildRootCell( points, halfThickness );	
+	const int maxCellRadius = (int) ceil( rootCell.resolution / 2 / minResolution );
+	const int minCellRadius = (int) floor( (rootCell.resolution / 2 - 2 * halfThickness) / minResolution );
+
+	//const int oldMaxNumRefinementSteps = 8* maxCellRadius*maxCellRadius*maxCellRadius;
+	// max num of filled cells: all points with same position and distance (max overlap) and #cells = spherical shell
+	// not optimal with rounded results: 48533 estimated vs 185688 actually (radius 5, thickness 1, res 0.1)
+	const int maxNumRefinementSteps = int( 4.0 * M_PI /3.0 * (maxCellRadius*maxCellRadius*maxCellRadius - minCellRadius*minCellRadius*minCellRadius) );
+
+	// sorted ascending by upper bound	
+	std::vector<SparseCellInfo> cells, finishedCells;
+	cells.reserve( 1 + 7 * maxNumRefinementSteps );
+	finishedCells.reserve( 1 + 7 * maxNumRefinementSteps );
+
+	cells.push_back( std::move( rootCell ) );
+
+	int bestLowerBound = 0;
+	int formerBestLowerBound;
+
+	for( int refinementStep = 0 ; refinementStep < maxNumRefinementSteps ; ++refinementStep ) {
+		formerBestLowerBound = bestLowerBound;
+
+		SparseCellInfo parentCell;
+		// pop 
+		std::swap( parentCell, cells.back() );
+		cells.pop_back();
+
+		// refine the cell (split into 8)
+		const float resolution = parentCell.resolution / 2;
+		for( int corner = 0 ; corner < 8 ; corner++ ) {			
+			const Eigen::Vector3f offset = indexToCubeCorner[corner].cast<float>() * resolution;
+
+			const Eigen::Vector3f minCorner = parentCell.minCorner + offset;
+			const Eigen::Vector3f maxCorner = minCorner + Eigen::Vector3f::Constant( resolution );
+
+			SparseCellInfo cell;
+
+			cell.lowerBound = parentCell.lowerBound;
+			// reset to lower bound before we test whether any partial overlap has become a full one
+			cell.upperBound = parentCell.lowerBound;
+
+			for( int j = 0 ; j < parentCell.partialPointIndices.size() ; j++ ) {
+				const int pointIndex = parentCell.partialPointIndices[j];
+				const Point &point = points[pointIndex];
+
+				const float minSquaredDistance = squaredMinDistanceAABoxPoint( minCorner, maxCorner, point.center );
+				const float maxSquaredDistance = squaredMaxDistanceAABoxPoint( minCorner, maxCorner, point.center );
+
+				float minSphereSquaredDistance = (point.distance - halfThickness) * (point.distance - halfThickness);
+				float maxSphereSquaredDistance = (point.distance + halfThickness) * (point.distance + halfThickness);
+				if( maxSquaredDistance < minSphereSquaredDistance || maxSphereSquaredDistance < minSquaredDistance ) {
+					continue;
+				}
+				// at least partial
+				cell.upperBound++;
+
+				// full?
+				if( minSphereSquaredDistance <= minSquaredDistance && maxSquaredDistance <= maxSphereSquaredDistance ) {
+					cell.lowerBound++;
+					//cell.fullPointIndices.push_back( pointIndex );
+				}
+				else {
+					// only partial
+					cell.partialPointIndices.push_back( pointIndex );
+				}
+			}
+
+			// only keep cells that are a potential solution
+			if( cell.upperBound >= bestLowerBound && cell.upperBound > 0 ) {
+				cell.minCorner = minCorner;
+				cell.resolution = resolution;
+
+				bestLowerBound = std::max( bestLowerBound, cell.lowerBound );
+
+				if( cell.upperBound == cell.lowerBound ) {
+					finishedCells.push_back( cell );
+				}
+				else if( resolution <= minResolution ) {
+					finishedCells.push_back( cell );
+				}
+				else {
+					cells.push_back( cell );
+				}
+			}
+		}
+
+		//intermediateResults.push_back( cells );
+
+		// if the lower bound has changed filter all cells out with a worse upper bound
+		if( formerBestLowerBound != bestLowerBound ) {
+			filterCells( cells, [bestLowerBound](const SparseCellInfo &cell) { return cell.upperBound < bestLowerBound; } );
+		}
+
+		// we're done if no cells are left to refine here now
+		if( cells.empty() ) {
+			break;
+		}
+
+		// swap the cell with the best upper bound to the back
+		{
+			int bestIndex = 0;
+			int bestUpperBound = bestLowerBound;
+			for( int i = cells.size() - 1 ; i >= 0 ; --i ) {
+				const int upperBound = cells[i].upperBound;
+
+				if( upperBound >= bestUpperBound ) {
+					bestUpperBound = upperBound;
+					bestIndex = i;	
+
+					if( upperBound == parentCell.upperBound ) {
+						// this is the best we can achieve
+						break;
+					}
+				}
+			}
+			if( bestIndex != cells.size() - 1 ) {
+				std::swap( cells[bestIndex], cells.back() );
+			}
+		}
+	}
+
+	// filter the finishedCells
+	filterCells( finishedCells, [bestLowerBound](const SparseCellInfo &cell) { return cell.upperBound < bestLowerBound; } );	
+
+	// move the remaining cells
+	//std::move( cells.begin(), cells.end(), std::back_inserter( finishedCells ) );
+	return finishedCells;
+}
