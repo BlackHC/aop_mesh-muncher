1c2e5d6d28a79521ec57da03b7c441235d3265c7
Andreas Kirsch <blackhc@gmail.com>
2012-09-27 20:48:47 +0200

    framework: add tests for mathUtility.h

---
+++ framework/test_rayIntersections.cpp
@@ -0,0 +1,104 @@
+#include "mathUtility.h"
+
+#include <gtest.h>
+
+using namespace Eigen;
+
+TEST(rayIntersections_unitCube, mainAxis ) {
+	Vector3f hitPoint;
+	EXPECT_TRUE( intersectRayWithUnitCube( Vector3f::UnitX() * 2, -Vector3f::UnitX(), hitPoint ) );
+	EXPECT_TRUE( intersectRayWithUnitCube( Vector3f::UnitY() * 2, -Vector3f::UnitY(), hitPoint ) );
+	EXPECT_TRUE( intersectRayWithUnitCube( Vector3f::UnitZ() * 2, -Vector3f::UnitZ(), hitPoint ) );
+
+	EXPECT_TRUE( intersectRayWithUnitCube( -Vector3f::UnitX() * 2, Vector3f::UnitX(), hitPoint ) );
+	EXPECT_TRUE( intersectRayWithUnitCube( -Vector3f::UnitY() * 2, Vector3f::UnitY(), hitPoint ) );
+	EXPECT_TRUE( intersectRayWithUnitCube( -Vector3f::UnitZ() * 2, Vector3f::UnitZ(), hitPoint ) );
+
+	// false: x
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitX() * 2, Vector3f::UnitY(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitX() * 2, Vector3f::UnitZ(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitX() * 2, -Vector3f::UnitY(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitX() * 2, -Vector3f::UnitZ(), hitPoint ) );
+
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitX() * 2, Vector3f::UnitY(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitX() * 2, Vector3f::UnitZ(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitX() * 2, -Vector3f::UnitY(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitX() * 2, -Vector3f::UnitZ(), hitPoint ) );
+
+	// false: y
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitY() * 2, Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitY() * 2, Vector3f::UnitZ(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitY() * 2, -Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitY() * 2, -Vector3f::UnitZ(), hitPoint ) );
+
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitY() * 2, Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitY() * 2, Vector3f::UnitZ(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitY() * 2, -Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitY() * 2, -Vector3f::UnitZ(), hitPoint ) );
+
+	// false: z
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitZ() * 2, Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitZ() * 2, Vector3f::UnitY(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitZ() * 2, -Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( Vector3f::UnitZ() * 2, -Vector3f::UnitY(), hitPoint ) );
+
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitZ() * 2, Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitZ() * 2, Vector3f::UnitY(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitZ() * 2, -Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithUnitCube( -Vector3f::UnitZ() * 2, -Vector3f::UnitY(), hitPoint ) );
+}
+
+#if 0
+TEST(rayIntersections_unitCube, hitPointCheck ) {
+	for( int ix = -100 ; ix <= 100 ; ++ix )
+		for( int iy = -100 ; iy <= 100 ; ++iy )
+			for( int iz = -100 ; iz <= 100 ; ++iz ) {
+				const Vector3f position( ix / 10.0, iy / 10.0, iz / 10.0 );
+
+				Vector3f hitPoint;
+				EXPECT_TRUE(  intersectRayWithUnitCube( position, -position, hitPoint ) );
+
+				EXPECT_TRUE( hitPoint.cwiseAbs().maxCoeff() < 1.1f );
+			}
+}
+#endif
+
+TEST(rayIntersections_box, mainAxis ) {
+	
+	const Vector3f offset(100, 50, -75 );
+	const Vector3f size(8, 4, 2);
+	const AlignedBox3f box( offset - size / 2, offset + size / 2 );
+
+	Vector3f hitPoint;
+	// xy
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 5, 0, 0 ), -Vector3f::UnitX(), hitPoint ) );
+	
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 5, 1, 0 ), -Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithAABB( box, offset + Vector3f( 5, 3, 0 ), -Vector3f::UnitX(), hitPoint ) );
+
+	// xz
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 5, 0, 0 ), -Vector3f::UnitX(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithAABB( box, offset + Vector3f( 5, 0, 2 ), -Vector3f::UnitX(), hitPoint ) );
+
+	// yx
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 0, 5, 0 ), -Vector3f::UnitY(), hitPoint ) );
+
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 3, 5, 0 ), -Vector3f::UnitY(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithAABB( box, offset + Vector3f( 5, 5, 0 ), -Vector3f::UnitY(), hitPoint ) );
+
+	// yz
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 0, 5, 0 ), -Vector3f::UnitY(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithAABB( box, offset + Vector3f( 0, 5, 2 ), -Vector3f::UnitY(), hitPoint ) );
+
+	// zx
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 0, 0, 5 ), -Vector3f::UnitZ(), hitPoint ) );
+
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 3, 0, 5 ), -Vector3f::UnitZ(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithAABB( box, offset + Vector3f( 5, 0, 5 ), -Vector3f::UnitZ(), hitPoint ) );
+
+	// zy
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 0, 0, 5 ), -Vector3f::UnitZ(), hitPoint ) );
+
+	EXPECT_TRUE( intersectRayWithAABB( box, offset + Vector3f( 0, 1, 5 ), -Vector3f::UnitZ(), hitPoint ) );
+	EXPECT_FALSE( intersectRayWithAABB( box, offset + Vector3f( 0, 3, 5 ), -Vector3f::UnitZ(), hitPoint ) );
+}
